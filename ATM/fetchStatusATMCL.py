9e90f769b701a73f104df7c5c573e53a37734711
Jansen A. Simanullang <jansen.simanullang@gmail.com>
2017-07-07 09:47:16 +0700

    Update fetchStatusATMCL.py

--- ATM/fetchStatusATMCL.py
+++ ATM/fetchStatusATMCL.py
@@ -1,28 +1,39 @@
 #!/usr/bin/python
 #---------------------------------------
-# fetchStatusATMCL.py
+# ffetchStatusATMCL.py
 # (c) Jansen A. Simanullang, 11:15:55
-#     updated 22.10.2015 17:05
+# @BSD City: Januari - November 2016
+# @Medan City: June - July 2017
 # to be used with telegram-bot plugin
 #---------------------------------------
-# usage: fetchStatusATM 1234567
-# script name followed by space and TID/UKO/CRO
+# usage #1: 
+# ffetchStatusATMCL cro/uko/kode cabang
+# script name followed by cro/uko or branchCode
+# usage #2: ffetchStatusATMCL [cro/uko/kode cabang] [telegram_id]
+# to send to telegram id directly
 #---------------------------------------
 
 from BeautifulSoup import BeautifulSoup
-import os, sys, time, ConfigParser
+import os, sys, time
 import urllib2
-from helperlibrary import *
+from operator import itemgetter
 #-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 scriptDirectory = os.path.dirname(os.path.abspath(__file__)) + "/"
 from loadConfig import readConfig
-regionID = readConfig("Atmpro")['regionid'].upper()
+from helperlibrary import *
+atmproIP = readConfig("Atmpro")['ipaddress']
+regionID = readConfig("Atmpro")['regionid']
 regionName = readConfig("Atmpro")['regionname']
-strHeaderLine = "\n---------------------------------\n"
+secretKey = readConfig("Telegram")['token']
+Telebot = readConfig("Telegram")['username']
+textLimit = int(readConfig("Telegram")['textlimit'])
+behindProxy=int(readConfig("Internet")['behindproxy'])
+strHeaderLine = "\n----------------------------------------------\n"
 #-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 
 
-def getATMStats(table, AREAID):
+
+def getTCashLow(table):
 
 	soup = BeautifulSoup(str(table))
 	
@@ -34,9 +45,9 @@ def getATMStats(table, AREAID):
 
 	numRowsHead = getRowsHeadNumber(table)
 
-	#print numRowsHead, numRows
-	msgBody = ""
-	counterATM = 0
+	#Initialize
+
+	TCashLow = []
 	
 	for i in range (0, numRows):
 
@@ -44,161 +55,241 @@ def getATMStats(table, AREAID):
 
 		tdcells = trs.findAll("td")
 		thcells = trs.findAll("th")
-
-		#print len(tdcells), len(thcells)
-
-
-
-		if tdcells and (AREAID in tdcells[7].getText()):
-			counterATM +=1
-		
-			msgBody += "\n"+str(counterATM)+") "+ tdcells[1].getText()+", " + tdcells[2].getText().replace("HYOSUNG","HYOSUNG ") +"\n\nLOKASI: "+ tdcells[5].getText() +"\nAREA: "+ tdcells[6].getText()+"\nDURASI: "+ durasiLastTunai(tdcells[11].getText()) +"\nKETERANGAN:\n"+ tdcells[8].getText().strip() +"\n"
-
-	if msgBody == "":
-		msgBody = "Tidak ada ATM CL dalam kategori ini di wilayah kerja Anda."
-
-	return msgBody
-
-
-
-def getATMStatsCRO(table, AREAID):
-
-	soup = BeautifulSoup(str(table))
-	
-	rows = soup.findAll('tr')
-
-	numRows = getRowsNumber(table)
-
-	numCols = getColsNumber(table)
-
-	numRowsHead = getRowsHeadNumber(table)
-
-	#print numRowsHead, numRows
-	msgBody = ""
-
-	seqNo = 0
-
-	for i in range (0, numRows):
-
-		trs = BeautifulSoup(str(rows[i]))
-
-		tdcells = trs.findAll("td")
-		thcells = trs.findAll("th")
-
-		#print len(tdcells), len(thcells)
-
-
 
 		if tdcells:
+			strTID = tdcells[1].getText()
+			strLocation = tdcells[5].getText()
+			strLocation = cleanUpLocation(strLocation)
+			strArea = tdcells[6].getText()
+			datLastTunai = tdcells[11].getText().strip()
 
-			if "ATM CENTER" in tdcells[6].getText():
-
-				seqNo = seqNo +1
-
-				msgBody += "\n"+str(seqNo)+") " + "CRO: "+ tdcells[6].getText().replace("ATM CENTER","").replace("(","").replace(")","")+ "\n\nTID: " +tdcells[1].getText()+", " + tdcells[2].getText().replace("HYOSUNG","HYOSUNG ") + "\nPENGELOLA: " + tdcells[7].getText()+"\nLOKASI: "+ tdcells[5].getText() +"\nDURASI: "+ durasiLastTunai(tdcells[11].getText()).replace("days", "hari ").replace("hours","jam") + "\n"
-
-				if tdcells[9].getText():
-
-					msgBody += "KETERANGAN:\n"+ tdcells[8].getText().lower() + "\n"
-
-	if msgBody == "":
-		msgBody = "Tidak ada ATM CL CRO kategori ini di wilayah kerja Anda."
-
-	return msgBody
+			if ("ATM CENTER" in strArea) or ("VENDOR" in strArea):
+				intCRO = 1
+				namaCROUKO = cleanUpNamaCRO(strArea)
+			else:
+				intCRO = 0
+				namaCROUKO = cleanupNamaUker(strArea)
+				
+			strCabang = tdcells[7].getText()
+			strKodeCabang = strCabang[:4]
+			strNamaCabang = strCabang[6:]
 
 
-def getATMStatsUKO(table, AREAID):
+			TCashLow.append((strKodeCabang, namaCROUKO, intCRO, strArea, strLocation, strTID, strNamaCabang, datLastTunai))
+			#TCashLow       ((#0:strKodeCabang, #1:namaCROUKO, #2:intCRO, #3:strArea, #4:strLocation, #5:strTID, #6:strNamaCabang, #7:datLastTunai))
 
-	soup = BeautifulSoup(str(table))
-	
-	rows = soup.findAll('tr')
+	TCashLow = sorted(TCashLow, key=itemgetter(1, 3, 2), reverse = False)
 
-	numRows = getRowsNumber(table)
+	return TCashLow 
 
-	numCols = getColsNumber(table)
 
-	numRowsHead = getRowsHeadNumber(table)
+def getTCashLowCabang(TCashLow, branchCode):
 
-	#print numRowsHead, numRows
+	#Initialize
 	msgBody = ""
-
-	seqNo = 0
 	
-	for i in range (0, numRows):
+	TCashLowKanca = []
+	TCashLowUKO = []
+	TCashLowCRO = []
 
-		trs = BeautifulSoup(str(rows[i]))
+	for i in range(0, len(TCashLow)):
+		if TCashLow[i][0] == branchCode.zfill(4):
+			strNamaCabang = cleanupNamaUker(TCashLow[i][6])
+			TCashLowKanca.append(TCashLow[i])
 
-		tdcells = trs.findAll("td")
-		thcells = trs.findAll("th")
+	for i in range(0, len(TCashLowKanca)):
+		if TCashLowKanca[i][2] == 0:
+			TCashLowUKO.append((TCashLowKanca[i][3], TCashLowKanca[i][5]))
 
-		#print len(tdcells), len(thcells)
+	for i in range(0, len(TCashLowKanca)):
+		if TCashLowKanca[i][2] == 1:
+			TCashLowCRO.append((TCashLowKanca[i][1], TCashLowKanca[i][4], TCashLowKanca[i][5]))
+
+	if TCashLowUKO or TCashLowCRO:
+		msgBody = strHeaderLine +"ATM CASH LOW "+ strNamaCabang.upper() +timestamp+ strHeaderLine + msgBody + "\n"
+	else:
+
+		msgBody = strHeaderLine +"ATM CASH LOW "+ branchCode +timestamp+ strHeaderLine + msgBody + "\nEXCELLENT WORK!\nALL ONLINE ATM IS READY TO DISPENSE MONEY!"
+
+	if TCashLowUKO:
+		msgBody += "*[UKO]*\n"
+		for i in range(0, len(TCashLowUKO)):
+					msgBody += str(i+1)+" "+ str(TCashLowUKO[i][0])+", "+str(TCashLowUKO[i][1])+"\n"
+		msgBody += "\n"			
+
+	if TCashLowCRO:
+		msgBody += "*[CRO]*\n"
+		for i in range(0, len(TCashLowCRO)):
+					msgBody += str(i+1)+" "+ str(TCashLowCRO[i][0]) +": "+str(TCashLowCRO[i][1])+", "+str(TCashLowCRO[i][2])+"\n"
+
+	return 	msgBody
 
 
+def getTCashLowCRO(TCashLow, selectedCRO):
 
-		if tdcells:
+	#Initialize
+	msgBody = ""
+	seqNo = 0
+	counter = 0
+	TCashLowCRO = []
+	arrCRO = ["BG", "G4S", "KEJAR", "SSI", "TAG"]
+
+	if selectedCRO == 0:
+
+		selectedCRO = "ALL"
+		strCRO = "*[ALL CRO]*"
+
+		for i in range(0, len(TCashLow)):
+			if TCashLow[i][2] == 1:
+				strNamaCabang = cleanupNamaUker(TCashLow[i][-1])
+				TCashLowCRO.append((TCashLow[i][1], TCashLow[i][4], TCashLow[i][5], TCashLow[i][7]))
 
 
-			if ("ATM CENTER" not in tdcells[6].getText()) and ("VENDOR" not in tdcells[6].getText()):
+	elif selectedCRO in arrCRO:
 
-				seqNo = seqNo +1
+		arrCRO = [""]
+		arrCRO.append(selectedCRO)
+		strCRO = "*["+selectedCRO+"]*"
+	
+		for i in range(0, len(TCashLow)):
 
-				msgBody += "\n"+str(seqNo)+") "+ tdcells[7].getText().upper() + "\n\nTID: " + tdcells[1].getText()+", " + tdcells[2].getText().replace("HYOSUNG","HYOSUNG ") +"\nLOKASI: "+ tdcells[5].getText() +"\nDURASI: "+ durasiLastTunai(tdcells[11].getText()).replace("days", "hari ").replace("hours","jam") +"\n"
+			if TCashLow[i][1] == selectedCRO:
+				strNamaCabang = cleanupNamaUker(TCashLow[i][-1])
+				TCashLowCRO.append((TCashLow[i][1], TCashLow[i][4], TCashLow[i][5], TCashLow[i][7]))
 
-				if tdcells[9].getText():
+	if TCashLowCRO:
 
-					msgBody += "KETERANGAN:\n"+ tdcells[8].getText().lower() + "\n"
+		msgBody += strCRO+"\n"
+		TCashLowCRO = sorted(TCashLowCRO, key=itemgetter(0,3), reverse = False)
+		for i in range(0, len(TCashLowCRO)):
 
-	if msgBody == "":
-		msgBody = "Tidak ada ATM CL UKO kategori ini di wilayah kerja Anda."
+			if TCashLowCRO[i-1][0] != TCashLowCRO[i][0]:
+				msgBody += "\n*[" + str(TCashLowCRO[i][0]) + "]*\n"
+				seqNo = 0
 
-	return msgBody
+			for j in range(0, len(arrCRO)):
+
+				if str(TCashLowCRO[i][0]) == arrCRO[j]:
+					seqNo += 1
+					counter += 1
+					msgBody += str(seqNo)+") "+ str(TCashLowCRO[i][1])+", "+str(TCashLowCRO[i][2])+", "+durasiLastTunai(str(TCashLowCRO[i][3]))+"\n"
 
 
+		msgBody += "\n"+regionName + "-[TOTAL CASH LOW CRO "+selectedCRO+"]: "+str(counter)
+
+	return 	msgBody
+
+
+def getTCashLowUKO(TCashLow):
+
+	#Initialize
+	msgBody = ""
+	seqNo = 0
+	counter = 0
+	TCashLowUKO = []
+
+	for i in range(0, len(TCashLow)):
+	
+		if TCashLow[i][2] == 0:
+			strNamaCabang = "*"+cleanupNamaUker(TCashLow[i][6])+"*"
+			TCashLowUKO.append((strNamaCabang.upper(), TCashLow[i][4], TCashLow[i][5], TCashLow[i][7]))
+			#TCashLow       ((#0:strKodeCabang, #1:namaCROUKO, #2:intCRO, #3:strArea, #4:strLocation, #5:strTID, #6:strNamaCabang, #7:datLastTunai))
+	TCashLowUKO = sorted(TCashLowUKO, key=itemgetter(0), reverse = False)
+
+	if TCashLowUKO:
+		TCashLowUKO = sorted(TCashLowUKO, key=itemgetter(0,3), reverse = False)
+		msgBody += "*[UKO]*\n"
+		for i in range(0, len(TCashLowUKO)):
+			if TCashLowUKO[i-1][0] != TCashLowUKO[i][0]:
+				msgBody +=  "\n"+str(TCashLowUKO[i][0]) + "\n"
+				seqNo = 0
+
+			seqNo += 1
+			counter += 1
+			msgBody += str(seqNo)+") "+ str(TCashLowUKO[i][1])+", "+str(TCashLowUKO[i][2])+", "+durasiLastTunai(str(TCashLowUKO[i][3]))+"\n"
+
+		msgBody += "\n"+regionName + "-[TOTAL CASH LOW UKO]: "+str(counter)
+
+	return 	msgBody
 
 msgBody =""
+
 timestamp = "\nper "+ time.strftime("%d-%m-%Y pukul %H:%M")
 
 if len(sys.argv) > 0:
 
-	AREAID = sys.argv[1]
+	alamatURL = "http://atmpro.bri.co.id/statusatm/viewbyregionprob.pl?REGID="+regionID+"&ERROR=CSHLOW_ST"
 
-	strHeader = "\n---------------------------------\n"
+	try:
+		AREAID = sys.argv[1]
+		try:
+			telegram_id = sys.argv[2]
+		except:
+			AREAID = sys.argv[1]
+			telegram_id = ""
 
-	if AREAID.isdigit():
+		if AREAID.isdigit():
 
-		alamatURL = "http://172.18.65.42/statusatm/viewbyregionprob.pl?REGID="+regionID+"&ERROR=CSHLOW_ST"
+			TCashLow = getTCashLow(table=getWidestTable(getTableList(fetchHTML(alamatURL))))
+			msgBody = getTCashLowCabang(TCashLow, AREAID)
 
-		msgBody = getATMStats(table=getWidestTable(getTableList(fetchHTML(alamatURL))), AREAID=AREAID)
+			if msgBody:	
+				print msgBody
+				if telegram_id:
+					TelegramTextSender(telegram_id, msgBody)
 
-		if msgBody:	
+	
+		if AREAID[0].isalpha():
 
-			msgBody = strHeader +"ATM CASH LOW - "+ AREAID + timestamp + strHeader + msgBody
-			print msgBody
+			if AREAID.upper() == "UKO":
 
+				try:
 
+					TCashLow = getTCashLow(table=getWidestTable(getTableList(fetchHTML(alamatURL))))
+					msgBody = getTCashLowUKO(TCashLow)
 
-	if AREAID.lower() == "cro":
+					if msgBody:	
+						msgBody = strHeaderLine +"*ATM CASH LOW "+ AREAID.upper() + "* - "+regionName+ timestamp+ strHeaderLine + msgBody
+						print msgBody.replace("**","") # double asterisk mark ("**") due to orphaned TID
 
-		alamatURL = "http://172.18.65.42/statusatm/viewbyregionprob.pl?REGID="+regionID+"&ERROR=CSHLOW_ST"
+						if telegram_id:
+							TelegramTextSender(telegram_id, msgBody)
 
-		msgBody = getATMStatsCRO(table=getWidestTable(getTableList(fetchHTML(alamatURL))), AREAID=AREAID)
+				except:
+					print "Ada kesalahan."
 
-		if msgBody:	
+			elif AREAID.upper() == "CRO":
 
-			msgBody = strHeader +"ATM CASH LOW - "+ AREAID.upper() + timestamp + strHeader + msgBody
-			print msgBody
+				try:
 
+					TCashLow = getTCashLow(table=getWidestTable(getTableList(fetchHTML(alamatURL))))
+					msgBody = getTCashLowCRO(TCashLow, 0)
 
+					if msgBody:	
+						msgBody = strHeaderLine +"*ATM CASH LOW "+ AREAID.upper() + "* - "+regionName+ timestamp+ strHeaderLine + msgBody
+						print msgBody
+						if telegram_id:
+							TelegramTextSender(telegram_id, msgBody)
 
+				except:
+					print "Ada kesalahan."
 
-	if AREAID.lower() == "uko":
+			else:
 
-		alamatURL = "http://172.18.65.42/statusatm/viewbyregionprob.pl?REGID="+regionID+"&ERROR=CSHLOW_ST"
+				try:
 
-		msgBody = getATMStatsUKO(table=getWidestTable(getTableList(fetchHTML(alamatURL))), AREAID=AREAID)
+					TCashLow = getTCashLow(table=getWidestTable(getTableList(fetchHTML(alamatURL))))
+					msgBody = getTCashLowCRO(TCashLow, AREAID.upper())
 
-		if msgBody:	
+					if msgBody:	
+						msgBody = strHeaderLine +"*ATM CASH LOW "+ AREAID.upper() + "* - "+regionName+ timestamp+ strHeaderLine + msgBody
+						print msgBody
+						if telegram_id:
+							TelegramTextSender(telegram_id, msgBody)
 
-			msgBody = strHeader +"ATM CASH LOW - "+ AREAID.upper() + timestamp + strHeader + msgBody
-			print msgBody
+				except:
+					print "CRO tidak dikenal."
 
+	except:
+
+		print "ANDA belum meng-input kode UKO/ CRO."
